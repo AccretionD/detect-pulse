b57595a053d72aabaaa4c531757ff0d42754c903
Guy Hughes <g@gxg.me>
2016-01-24 01:21:29 +0000

    cleanup

--- pulse.py
+++ pulse.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+
 from lib.device import VideoStream
 from lib.processors import findFaceGetPulse
 from lib.interface import plotXY, imshow, waitKey,destroyWindow, moveWindow
@@ -13,8 +15,6 @@ class Pulse(object):
             raise Exception("Filename unset")
 
         self.camera = VideoStream(filename=filename) #first camera by default
-        #self.w,self.h = 0,0
-        #self.pressed = 0
         self.bpms = []
 
         #Containerized analysis of recieved image frames (an openMDAO assembly)
@@ -30,15 +30,6 @@ class Pulse(object):
                                           data_spike_limit = 2500.,
                                           face_detector_smoothness = 10.)
 
-        #Init parameters for the cardiac data plot
-        #self.bpm_plot = False
-        #self.plot_title = "Cardiac info - raw signal, filtered signal, and PSD"
-
-        #Maps keystrokes to specified methods
-        #(A GUI window must have focus for these to work)
-        #self.key_controls = {"s" : self.toggle_search,
-        #                     "d" : self.toggle_display_plot,
-        #                     "f" : self.write_csv}
 
         while (self.main_loop()):
             sys.stderr.write(". ")
@@ -70,45 +61,6 @@ class Pulse(object):
         	self.processor.fft.reset()
         print "face detection lock =",not state
 
-    def toggle_display_plot(self):
-        """
-        Toggles the data display.
-        """
-        if self.bpm_plot:
-            print "bpm plot disabled"
-            self.bpm_plot = False
-            destroyWindow(self.plot_title)
-        else:
-            print "bpm plot enabled"
-            self.bpm_plot = True
-            self.make_bpm_plot()
-            moveWindow(self.plot_title, self.w,0)
-
-    def make_bpm_plot(self):
-        """
-        Creates and/or updates the data display
-        """
-        plotXY(
-            [
-                [self.processor.fft.times,
-                 self.processor.fft.samples],
-                [self.processor.fft.even_times[4:-4], 
-                 self.processor.measure_heart.filtered[4:-4]],
-                [self.processor.measure_heart.freqs, 
-                 self.processor.measure_heart.fft]
-            ],
-               labels = [False, False, True],
-               showmax = [False,False, "bpm"],
-
-               label_ndigits = [0,0,0],
-               showmax_digits = [0,0,1],
-
-               skip = [3,3,4],
-               name = self.plot_title, 
-               bg = self.processor.grab_faces.slices[0]
-        )
-
-        return self.processor.measure_heart.bpm
 
     def capture_bpms(self):
         try:
@@ -120,23 +72,6 @@ class Pulse(object):
             return
 
 
-    def key_handler(self):    
-        """
-        Handle keystrokes, as set at the bottom of __init__()
-
-        A plotting or camera frame window must have focus for keypresses to be
-        detected.
-        """
-
-        self.pressed = waitKey(10) & 255 #wait for keypress for 10 ms
-        if self.pressed == 27: #exit program on 'esc'
-            print "exiting..."
-            self.camera.cam.release()
-            exit()
-
-        for key in self.key_controls.keys():
-            if chr(self.pressed) == key:
-                self.key_controls[key]()
 
     def main_loop(self):
         """
